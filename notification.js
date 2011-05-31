8d54eff49f6556c166b6c1ef252cab51cbf5f56c
kawasima <kawasima1016@gmail.com>
2011-05-31 18:26:35 +0900

    add some options

--- notification.js
+++ notification.js
@@ -1,27 +1,37 @@
 var settings = {
-	get apiKey() {
-		if(!localStorage["apiKey"]) return "";
-		return localStorage["apiKey"];
-	},
-	set apiKey(val) {
-		localStorage["apiKey"] = val;
-	},
-	get redmineUrl() {
-		if(!localStorage["redmineUrl"]) return "";
-		return localStorage["redmineUrl"];
-	},
-	set redmineUrl(val) {
-		localStorage["redmineUrl"] = val;
-	},
-	get query() {
-		if(!localStorage["query"])
-			return "assigned_to_id=me&set_filter=1&sort=updated_on%3Adesc";
-		return localStorage["query"];
-	},
-	set query(val) {
-		localStorage["query"] = val;
-	}
+    get apiKey() {
+	if(!localStorage["apiKey"]) return "";
+	return localStorage["apiKey"];
+    },
+    set apiKey(val) {
+	localStorage["apiKey"] = val;
+    },
+    get redmineUrl() {
+	if(!localStorage["redmineUrl"]) return "";
+	return localStorage["redmineUrl"];
+    },
+    set redmineUrl(val) {
+	localStorage["redmineUrl"] = val;
+    },
+    get query() {
+	if(!localStorage["query"])
+	    return "assigned_to_id=me&set_filter=1&sort=updated_on%3Adesc";
+	return localStorage["query"];
+    },
+    set query(val) {
+	localStorage["query"] = val;
+    },
+    get checkInterval() {
+	if(!localStorage["checkInterval"]) return 10;
+	return localStorage["checkInterval"]);
+    },
+set checkInterval(val) {
+    localStorage["checkInterval"] = val;
 }
+
+    
+}
+
 function getBaseUrl() {
 	return settings.redmineUrl;
 }
